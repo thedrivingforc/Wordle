670513ba978b61ec4ec4bb17000f5de4a0ca9db1
Kumar Mritunjay <mritrunjay@gmail.com>
2022-07-27 11:52:42 +0530

    setting up homeScreen

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { colors } from "./src/constants";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+
+      <Text style={styles.title}>WORDLE</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.black,
     alignItems: "center",
-    justifyContent: "center",
+  },
+  title: {
+    color: colors.lightgrey,
+    fontSize: 32,
+    fontWeight: "bold",
+    letterSpacing: 7,
   },
 });
