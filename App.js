f551976392341e5a932a6e8d8c50f33853c06d81
Kumar Mritunjay <mritrunjay@gmail.com>
2022-07-27 12:20:22 +0530

    Game Board Component added

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from "react-native";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import { colors } from "./src/constants";
 import Keyboard from "./src/components/Keyboard";
 
+const NUMBER_OF_TRIES = 6;
+
 export default function App() {
+  const word = "hello";
+  const letters = word.split(""); // ["h", "e", "l", "l", "o"]
+
+  const rows = new Array(NUMBER_OF_TRIES).fill(
+    new Array(letters.length).fill("A")
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="light" />
 
       <Text style={styles.title}>WORDLE</Text>
+
+      <ScrollView style={styles.map}>
+        {rows.map((row) => (
+          <View style={styles.row}>
+            {row.map((cell) => (
+              <View style={styles.cell}>
+                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+
       <Keyboard />
     </SafeAreaView>
   );
@@ -21,9 +43,35 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   title: {
+    marginVertical: 20, // TODO: remove
     color: colors.lightgrey,
     fontSize: 32,
     fontWeight: "bold",
     letterSpacing: 7,
+  },
+  map: {
+    marginVertical: 35, // TODO: expected 20
+    alignSelf: "stretch",
+    height: 100,
+  },
+  row: {
+    alignSelf: "stretch",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  cell: {
+    borderWidth: 3,
+    borderColor: colors.darkgrey,
+    flex: 1,
+    maxWidth: 70,
+    aspectRatio: 1,
+    margin: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellText: {
+    color: colors.lightgrey,
+    fontSize: 28,
+    fontWeight: "bold",
   },
 });
